5cc0ca29b74cb1a43a29a245cf021e5c288e7e65
neswill1 <nenadkonatar@live.ac.me>
2015-04-29 13:11:49 +0200

    First commit

---
+++ settings.php
@@ -0,0 +1,8 @@
+<?php
+	$adresa="2340 Pine Ridge Rd, Naples, FL 34109"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad="Naples";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica=""; // AKo ne znate skracenicu ostavite praznu
+	$telephone=2392633123;// Broj koji se poziva na dugme CALL US
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="fade"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup"
+?>
